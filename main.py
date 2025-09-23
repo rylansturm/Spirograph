f47ed64f70e3409f48f4263f5f800e6749ac32d5
Rylan Sturm <rylan.sturm@gmail.com>
2025-09-22 20:39:42 -0700

    added colors

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 import turtle
 import math
 
-'''
+"""
 Simulation Parameters
-'''
-RESOLUTION = 50  # Higher number gives a higher resolution, but runs slower
+"""
+RESOLUTION = 80  # Higher number gives a higher resolution, but runs slower
 SIM_STEPS = 20000  # How many total steps to run
 RADS_PER_STEP = (2 * math.pi) / RESOLUTION  # Don't change
+DO_COLORS = True
 
 
-''' 
+""" 
 Spirograph Parameters
     In reality, p < r < R
     In this simulation, feel free to play
     with values that don't conform to that!
-'''
-R = 400  # Radius of outer ring
-r = 205  # Radius of inner cog
-p = 50  # Distance between pen hole and center of inner cog
+"""
+R = 600  # Radius of outer ring
+r = 407  # Radius of inner cog
+p = 256  # Distance between pen hole and center of inner cog
 
 
 def main():
+    turtle.width(3)
     turtle.speed(0)
     turtle.up()
     turtle.goto((R - r + p, 0.0))
@@ -38,6 +40,13 @@ def main():
         x = x_c + x
         y = y_c + y
 
+        if DO_COLORS:
+            # Play around with these!
+            red = (1 + math.sin(x / 100)) * 0.5
+            green = (1 + math.cos(y / 100)) * 0.5
+            blue = (1 + math.sin((x + y) / 100)) * 0.5
+            color = (red, green, blue)
+            turtle.pencolor(color)
         turtle.goto((x, y))
 
     input("Press enter to exit")
